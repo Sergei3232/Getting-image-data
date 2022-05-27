dedf3d9176c2bbaba73621ca9b0dd026f4a51aa6
sebashmakov <sebashmakov@ozon.ru>
2022-05-28 00:17:21 +0300

    add NewProcessor

--- internal/app/processor/processor.go
+++ internal/app/processor/processor.go
@@ -1,6 +1,7 @@
 package processor
 
 import (
+	"github.com/Sergei3232/Getting-image-data/config"
 	counter_id "github.com/Sergei3232/Getting-image-data/internal/app/counter"
 	csvCounter "github.com/Sergei3232/Getting-image-data/internal/app/csv"
 	"github.com/Sergei3232/Getting-image-data/internal/app/db"
@@ -10,5 +11,27 @@ type Processor struct {
 	Counter   counter_id.TextCounter
 	DbFileStr db.Repository
 	DbImage   db.Repository
-	CsvClient csvCounter.HandlersCsv
+	CsvClient *csvCounter.ClientCsv
+}
+
+func NewProcessor(config config.WorkFile) (*Processor, error) {
+	counter, err := counter_id.NewCounter(config.PathCountFile, 0)
+	if err != nil {
+		return nil, err
+	}
+	csvCounter := csvCounter.NewCounterCsv()
+	dbFileStr, err := db.NewDbConnectClient(config.DNSFileLoader)
+	if err != nil {
+		return nil, err
+	}
+	ddbImage, err := db.NewDbConnectClient(config.DNSImageLoader)
+	if err != nil {
+		return nil, err
+	}
+	return &Processor{
+		Counter:   counter,
+		CsvClient: csvCounter,
+		DbImage:   ddbImage,
+		DbFileStr: dbFileStr,
+	}, nil
 }

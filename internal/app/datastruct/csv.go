528a7b336e69daf4a0731d56ac2c39ab010d9e68
sebashmakov <sebashmakov@ozon.ru>
2022-05-29 00:24:57 +0300

    add type DataSCV struct

--- internal/app/datastruct/csv.go
+++ internal/app/datastruct/csv.go
@@ -1,8 +1,16 @@
 package datastruct
 
 type ImageFileCSV struct {
-	Sku      int64
-	MapiItem int64
-	Height   int64
-	Width    int64
+	Sku           int64
+	MapiItem      int64
+	Height        int64
+	Width         int64
+	IdFileStorage int64
+}
+
+type DataSCV struct {
+	Id            int64
+	IdFileStorage int64
+	Height        int64
+	Width         int64
 }
